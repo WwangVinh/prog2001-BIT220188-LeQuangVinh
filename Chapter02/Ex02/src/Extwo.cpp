3fd6ce35a0c7cb22f2cfc6145562136b9b7b412f
WwangVinh <wwangvinh04@gmail.com>
2026-04-13 22:12:11 +0700

    Create Extwo.cpp

---
+++ Chapter02/Ex02/src/Extwo.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float pi = 3.14159;
+
+    printf("So pi Mac dinh: %f\n", pi);
+    printf("Lay 1 chu so: %.1f\n", pi);
+    printf("Lay 4 chu so: %.4f\n", pi);
+
+    return 0;
+}
