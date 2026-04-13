4d5529e955697a8d724a27772e469ae85c3627e2
WwangVinh <wwangvinh04@gmail.com>
2026-04-13 22:18:05 +0700

    Create ExFour.cpp

---
+++ Chapter02/Ex04/src/ExFour.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int diem_toan = 10;    // Hợp lệ: Dùng chữ cái và dấu gạch dưới
+    int age2026 = 21;      // Hợp lệ: Có thể có số ở sau
+    float _temp = 36.5;    // Hợp lệ: Có thể bắt đầu bằng dấu gạch dưới
+
+    // int 123diem = 10;   // SAI: Không được bắt đầu bằng chữ số
+    // float so luong = 5; // SAI: Không được chứa khoảng trắng
+    // char char = 'A';    // SAI: Không được trùng với từ khóa hệ thống (char, int,...)
+
+    printf("Quy tac dat ten bien!\n");
+    return 0;
+}
